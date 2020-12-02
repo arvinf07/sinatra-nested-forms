425dac4076b62482df97dfa966a93dde5a14b648
Arvin Fernandez <arvinf07@gmail.com>
2020-12-01 20:42:43 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,25 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@all = []
+
+  def initialize(attr_hash)
+    attr_hash.each do |key, value|
+      self.class.attr_accessor key.to_sym
+      self.send(("#{key}="), value)
+    end
+    save
+  end  
+
+  def self.clear
+    @@all = []
+  end  
+
+  def save
+    @@all << self
+  end  
+
+  def self.all
+    @@all
+  end  
+
 end
